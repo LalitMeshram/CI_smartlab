58612b0917d2166bd745f4dba382390d8fcfc54d
vbpawar <vikaspawar3110@gmail.com>
2021-06-27 14:33:24 +0530

    first commit

--- application/controllers/UserMasterController.php
+++ application/controllers/UserMasterController.php
@@ -11,9 +11,9 @@ class UserMasterController extends REST_Controller {
         $this->load->model('UserMasterModel', 'user');
     }
 
-    public function user_get($id = 0) {
+    public function customers_list_get($id = 0) {
         $response = [];
-        $data = $this->user->get_user($id);
+        $data = $this->user->get_customers($id);
         if (!empty($data)) {
             $response['data'] = $data;
             $response['msg'] = 'All Data Fetch successfully!';
@@ -22,28 +22,19 @@ class UserMasterController extends REST_Controller {
         } else {
             $response['msg'] = 'Data not Found!';
             $response['status'] = 404;
-            $this->response($response, REST_Controller::HTTP_NOT_FOUND);
+            $this->response($response, REST_Controller::HTTP_OK);
         }
     }
 
-    public function user_post() {
+    public function center_reg_post() {
         $response = [];
 
-        $data['business_name'] = $this->post('business_name');
-        $data['owner_name'] = $this->post('owner_name');
-        $data['emailid'] = $this->post('emailid');
-        $data['contact1'] = $this->post('contact1');
-        $data['contact2'] = $this->post('contact2');
-        $data['address'] = $this->post('address');
-        $data['highway'] = $this->post('highway');
-        $data['city'] = $this->post('city');
-        $data['is_active'] = $this->post('is_active');
-        $data['created_at'] = date(DATE_ISO8601, time());
-        $data['modified_at'] = date(DATE_ISO8601, time());
-
-        $id = $this->user->insert_user($data);
+        $data['emailId'] = $this->post('emailId');
+        $data['contact_number'] = $this->post('contact_number');
+        $data['upassword'] = $this->post('upassword');
+        $id = $this->user->customer_reg($data);
         if (!empty($id)) {
-            $response['msg'] = 'user created successfully!';
+            $response['msg'] = 'Customer Registration is successfully Done!';
             $response['id'] = $id;
             $response['status'] = 200;
             $this->response($response, REST_Controller::HTTP_OK);
@@ -51,41 +42,33 @@ class UserMasterController extends REST_Controller {
             $response['msg'] = 'Bad Request!';
             $response['id'] = $id;
             $response['status'] = 400;
-            $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+            $this->response($response, REST_Controller::HTTP_OK);
         }
     }
 
-    public function user_update_post() {
+    public function center_update_post() {
         $response = [];
         //if user id is present then execute if otherwise else block will execute
         if (!empty($this->user->get_user($this->post('uId')))) {
-            $data['id'] = $this->post('uId');
-            $data['business_name'] = $this->post('ubusiness_name');
-            $data['owner_name'] = $this->post('uowner_name');
-            $data['emailid'] = $this->post('uemailid');
-            $data['contact1'] = $this->post('ucontact1');
-            $data['contact2'] = $this->post('ucontact2');
-            $data['address'] = $this->post('uaddress');
-            $data['highway'] = $this->post('uhighway');
-            $data['city'] = $this->post('ucity');
-            $data['is_active'] = $this->post('uis_active');
-            $data['modified_at'] = date(DATE_ISO8601, time());
+            $data['centerId'] = $this->post('centerId');
+            $data['emailid'] = $this->post('emailid');
+            $data['contact_number'] = $this->post('contact_number');
+            $data['upassword'] = $this->post('upassword');
+            $this->user->update_customer_center($data);
 
-            $this->user->update_user($data);
-
-            $response['msg'] = 'user Updated successfully!';
+            $response['msg'] = 'Center Data Updated successfully!';
             $response['status'] = 200;
             $this->response($response, REST_Controller::HTTP_OK);
         } else {
             $response['msg'] = 'Data not Found!';
             $response['status'] = 404;
-            $this->response($response, REST_Controller::HTTP_NOT_FOUND);
+            $this->response($response, REST_Controller::HTTP_OK);
         }
     }
 
-    public function user_delete($id) {
+    public function center_deactivate_delete($id) {
         $response = [];
-        if (!empty($this->user->get_user($id))) {
+        if (!empty($this->user->get_customers($id))) {
             $result = $this->user->delete_user($id);
             if ($result == 1) {
                 $response['msg'] = 'user successfully deleted!';
@@ -94,13 +77,14 @@ class UserMasterController extends REST_Controller {
             } else {
                 $response['msg'] = 'Bad request!';
                 $response['status'] = 400;
-                $this->response($response, REST_Controller::HTTP_BAD_REQUEST);
+                $this->response($response, REST_Controller::HTTP_OK);
             }
         } else {
             $response['msg'] = 'Record not Found!';
             $response['status'] = 404;
-            $this->response($response, REST_Controller::HTTP_NOT_FOUND);
+            $this->response($response, REST_Controller::HTTP_OK);
         }
     
 
+}
 }
--- application/models/UserMasterModel.php
+++ application/models/UserMasterModel.php
@@ -2,25 +2,25 @@
 
 class UserMasterModel extends CI_Model {
 
-    public function insert_user($data) {
+    public function customer_reg($data) {
 
-        $this->db->insert('user_master', $data);
+        $this->db->insert('customer_registeration', $data);
         return $this->db->insert_id();
     }
 
-    public function get_user($id) {
+    public function get_customers($id) {
         $data;
         if ($id != 0) {
-            $data = $query = $this->db->get_where('user_master', array('id' => $id))->row_array();
+            $data = $query = $this->db->get_where('customer_registeration', array('centerId' => $id))->row_array();
         } else {
-            $data = $this->db->get('user_master')->result();
+            $data = $this->db->get('customer_registeration')->result();
         }
         return $data;
     }
 
-    public function update_user($data) {
-        $this->db->where('id', $data['id']);
-        $this->db->update('user_master', $data);
+    public function update_customer_center($data) {
+        $this->db->where('centerId', $data['id']);
+        $this->db->update('customer_registeration', $data);
     }
     
     public function delete_user($id) {
