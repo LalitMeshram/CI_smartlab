0e1d441988e702616e3788139dcb0b258a8bcc34
vbpawar <vikaspawar3110@gmail.com>
2021-07-05 10:54:20 +0530

    add db file

--- db/smartlab (1).sql
+++ db/smartlab (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2021 at 01:57 PM
+-- Generation Time: Jul 05, 2021 at 07:23 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -21,6 +21,58 @@ SET time_zone = "+00:00";
 --
 -- Database: `smartlab`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `center_details`
+--
+
+CREATE TABLE `center_details` (
+  `centerId` int(10) UNSIGNED NOT NULL,
+  `labName` varchar(150) NOT NULL,
+  `lab_contact` varchar(12) NOT NULL,
+  `lab_email` varchar(150) NOT NULL,
+  `lab_address` text NOT NULL,
+  `lab_city` varchar(150) NOT NULL,
+  `lab_postalcode` varchar(7) NOT NULL,
+  `createdat` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedat` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `center_details`
+--
+
+INSERT INTO `center_details` (`centerId`, `labName`, `lab_contact`, `lab_email`, `lab_address`, `lab_city`, `lab_postalcode`, `createdat`, `updatedat`) VALUES
+(1, 'Testing Laboratory', '9874589658', 'smith@got.com', 'Pune Maharshtra', 'Pune', '411004', '2021-07-04 10:51:22', '2021-07-04 10:56:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `center_letter_head_details`
+--
+
+CREATE TABLE `center_letter_head_details` (
+  `centerId` int(10) UNSIGNED NOT NULL,
+  `header_logo` varchar(255) DEFAULT NULL,
+  `lab_incharge_sign` varchar(255) DEFAULT NULL,
+  `doctor_sign` varchar(255) DEFAULT NULL,
+  `lab_incharge_name` varchar(150) NOT NULL,
+  `lab_incharge_degree` varchar(255) NOT NULL,
+  `lab_doctor_name` varchar(150) NOT NULL,
+  `lab_doctor_degree` varchar(255) NOT NULL,
+  `footer_logo` varchar(255) DEFAULT NULL,
+  `createdat` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedat` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `center_letter_head_details`
+--
+
+INSERT INTO `center_letter_head_details` (`centerId`, `header_logo`, `lab_incharge_sign`, `doctor_sign`, `lab_incharge_name`, `lab_incharge_degree`, `lab_doctor_name`, `lab_doctor_degree`, `footer_logo`, `createdat`, `updatedat`) VALUES
+(1, './documents/1.png2021_07_04_010128000000.png', './documents/3.png2021_07_04_010128000000.png', './documents/2.png2021_07_04_010128000000.png', 'John Smith', 'G.N.M', 'Dr.Nick Jonas', 'MBBS', './documents/11.jpg2021_07_04_010128000000.jpg', '2021-07-04 13:01:28', '2021-07-04 13:01:28');
 
 -- --------------------------------------------------------
 
@@ -43,7 +95,9 @@ CREATE TABLE `center_packages` (
 --
 
 INSERT INTO `center_packages` (`packageId`, `plan_name`, `amount`, `duration`, `isactive`, `createdat`, `updatedat`) VALUES
-(1, 'TRIAL PERIOD', 10.00, 5, 1, '2021-06-27 09:37:17', '2021-06-27 09:37:17');
+(1, 'TRIAL PERIOD', 10.00, 5, 1, '2021-06-27 09:37:17', '2021-06-27 09:37:17'),
+(2, 'Advanced Package', 250.00, 30, 1, '2021-07-04 13:06:20', '2021-07-04 13:06:20'),
+(3, 'VIP Customers', 500.00, 60, 1, '2021-07-04 13:06:20', '2021-07-04 13:06:20');
 
 -- --------------------------------------------------------
 
@@ -63,7 +117,11 @@ CREATE TABLE `center_package_details` (
 
 INSERT INTO `center_package_details` (`detailId`, `packageId`, `details`) VALUES
 (1, 1, '5 Days Free'),
-(2, 1, 'SMS Free');
+(2, 1, 'SMS Free'),
+(3, 2, '10 Days Extra Trial'),
+(4, 2, '20 Days Full access'),
+(5, 3, 'Get access of 10 days'),
+(6, 3, '50 Days Free');
 
 -- --------------------------------------------------------
 
@@ -88,8 +146,8 @@ CREATE TABLE `center_payment_details` (
 --
 
 INSERT INTO `center_payment_details` (`paymentId`, `centerId`, `packageId`, `startdate`, `enddate`, `paymentmode`, `payment_ref_number`, `createdat`, `updatedat`) VALUES
-(1, 1, 1, '2021-06-27', '2021-07-02', 'PhonePay', '123FRT', '2021-06-27 09:02:45', '2021-06-27 09:02:45'),
-(2, 1, 1, '2021-06-27', '2021-06-29', 'PhonePay', '123FRT', '2021-06-27 09:05:05', '2021-06-27 09:05:05');
+(1, 1, 1, '2021-06-27', '2021-06-28', 'PhonePay', '123FRT', '2021-06-27 09:02:45', '2021-06-29 21:08:33'),
+(2, 2, 1, '2021-06-27', '2021-06-29', 'PhonePay', '123FRT', '2021-06-27 09:05:05', '2021-06-29 21:14:37');
 
 -- --------------------------------------------------------
 
@@ -116,11 +174,25 @@ CREATE TABLE `customer_registeration` (
 
 INSERT INTO `customer_registeration` (`centerId`, `fullname`, `emailId`, `contact_number`, `upassword`, `isactive`, `ismailverified`, `ismobileverified`, `createdat`, `updatedat`) VALUES
 (1, 'John Smith', 'john@gmail.com', '9881658987', '12345', 1, 0, 0, '2021-06-26 16:46:41', '2021-06-28 17:27:40'),
-(2, 'Tony Stark', 'tonystark@avengers.com', '8208504868', '12345', 1, 0, 0, '2021-06-27 08:13:19', '2021-06-28 17:27:45');
+(2, 'Tony Stark', 'tonystark@avengers.com', '8208504868', '12345', 1, 0, 0, '2021-06-27 08:13:19', '2021-06-28 17:27:45'),
+(4, 'Sansa Stark', 'sansa@got.com', '9657856985', '12345', 1, 0, 0, '2021-07-02 08:39:40', '2021-07-02 08:39:40'),
+(5, 'Robb Stark', 'robb@got.com', '9657856985', '12345', 1, 0, 0, '2021-07-02 08:40:47', '2021-07-02 08:40:47');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `center_details`
+--
+ALTER TABLE `center_details`
+  ADD PRIMARY KEY (`centerId`);
+
+--
+-- Indexes for table `center_letter_head_details`
+--
+ALTER TABLE `center_letter_head_details`
+  ADD PRIMARY KEY (`centerId`);
 
 --
 -- Indexes for table `center_packages`
@@ -154,16 +226,28 @@ ALTER TABLE `customer_registeration`
 --
 
 --
+-- AUTO_INCREMENT for table `center_details`
+--
+ALTER TABLE `center_details`
+  MODIFY `centerId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `center_letter_head_details`
+--
+ALTER TABLE `center_letter_head_details`
+  MODIFY `centerId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `center_packages`
 --
 ALTER TABLE `center_packages`
-  MODIFY `packageId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `packageId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `center_package_details`
 --
 ALTER TABLE `center_package_details`
-  MODIFY `detailId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `detailId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `center_payment_details`
@@ -175,7 +259,7 @@ ALTER TABLE `center_payment_details`
 -- AUTO_INCREMENT for table `customer_registeration`
 --
 ALTER TABLE `customer_registeration`
-  MODIFY `centerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `centerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
