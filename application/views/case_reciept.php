31eb3042236ccb7db4ff99a56c808fe6db3d830f
vbpawar <vikaspawar3110@gmail.com>
2021-08-01 10:53:57 +0530

    add dompdf code

---
+++ application/views/case_reciept.php
@@ -0,0 +1,24 @@
+<?php
+/* include autoloader */
+require_once 'dompdf/autoload.inc.php';
+/* reference the Dompdf namespace */
+use Dompdf\Dompdf;
+
+/* instantiate and use the dompdf class */
+$dompdf = new Dompdf();
+
+$html = "<html><body><h1>Hi</h1></body></html>";
+set_time_limit(600);
+$dompdf->setPaper('A4', 'portrait');
+
+$dompdf->loadHtml($html);
+/* Render the HTML as PDF */
+$dompdf->render();
+
+/* Output the generated PDF to Browser */
+// $dompdf->stream();
+$dompdf->stream("payment.pdf", array(
+    "Attachment" => false
+));
+exit(0);
+?> 
