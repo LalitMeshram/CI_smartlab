38c7ff520ba0e77f7a7b039f25f42d57a1c201e0
lalit <lalit@DESKTOP-077QKLL>
2021-08-01 12:09:17 +0530

    loader

--- application/views/footer.php
+++ application/views/footer.php
@@ -262,7 +262,7 @@
         right: 0;
         bottom: 0;
         width: 100%;
-        background: rgba(0, 0, 0, 0.75) url('<?php echo base_url('db/image/loader.gif');?>') no-repeat center center;
+        background: rgba(0, 0, 0, 0.75) url('<?php echo base_url('resource/img/loader.gif');?>') no-repeat center center;
         z-index: 10000;
     }
     
