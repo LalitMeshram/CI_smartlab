64510679d0288651486af3d4b594f6bc26287666
vbpawar <vikaspawar3110@gmail.com>
2021-09-27 14:01:24 +0530

    add report model query

--- application/models/ReportModel.php
+++ application/models/ReportModel.php
@@ -4,11 +4,17 @@ class ReportModel extends CI_Model
 {
     
     public function get_finding_data($caseId){
-        $sql = "SELECT cm.categoryId,lc.category,cm.test_name,cs.test_name,cu.unit,10 lower,40 upper FROM case_tests ct 
+        // $sql = "SELECT cm.categoryId,lc.category,cm.test_name,cs.test_name,cu.unit,10 lower,40 upper FROM case_tests ct 
+        // INNER JOIN center_test_master cm ON cm.testId = ct.testId 
+        // INNER JOIN lab_center_categories lc ON lc.categoryid = cm.categoryId 
+        // INNER JOIN center_test_subtypes cs ON cs.testId = ct.testId 
+        // INNER JOIN center_unit_master cu ON cu.unitId = cs.unitId WHERE ct.caseId = $caseId";
+        $sql = "SELECT cm.categoryId,lc.category,cm.test_name,cp.testName,cu.unit,10 lower,40 upper,cs.isgroup,cs.label,cs.label,cs.flag_sequence FROM case_tests ct 
         INNER JOIN center_test_master cm ON cm.testId = ct.testId 
         INNER JOIN lab_center_categories lc ON lc.categoryid = cm.categoryId 
-        INNER JOIN center_test_subtypes cs ON cs.testId = ct.testId 
-        INNER JOIN center_unit_master cu ON cu.unitId = cs.unitId WHERE ct.caseId = $caseId";
+        INNER JOIN center_test_group_panel cs ON cs.testId = ct.testId
+        INNER JOIN center_test_panel cp ON cs.panelId = cp.panelId
+        INNER JOIN center_unit_master cu ON cu.unitId = cp.unitId WHERE ct.caseId = $caseId";
         $query = $this->db->query($sql);
         return $query->result();
     }
