77e6a068f64b8a88076961bd1ac00d6ee257569a
vbpawar <vikaspawar3110@gmail.com>
2021-10-12 15:17:24 +0530

    add report chnages

--- application/logs/log-2021-10-12.php
+++ application/logs/log-2021-10-12.php
@@ -1,58 +1,22 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-ERROR - 2021-10-12 12:05:17 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:18 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:37 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:37 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:48 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:48 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:52 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:52 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:52 --> 404 Page Not Found: Invoice/generate_invoice_bg.jpg
-ERROR - 2021-10-12 12:05:52 --> 404 Page Not Found: Images/favicon.ico
-ERROR - 2021-10-12 12:05:57 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:05:57 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:06:34 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:06:38 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', NULL, NULL, '10', '2', 'Chemistry', 'KG', 'Test one Test', '10-40', 1, '1')
-ERROR - 2021-10-12 12:06:38 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('2', 'Nurophils', NULL, NULL, '25', '2', 'Chemistry', 'KG', 'Test one Test', '10-40', 1, '1')
-ERROR - 2021-10-12 12:06:38 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', NULL, NULL, '2', '2', 'Chemistry', 'GM', '-', '10-40', 1, '0')
-ERROR - 2021-10-12 12:06:38 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', NULL, NULL, '25', '1', 'Physics', 'GM', '-', '10-40', 1, '0')
-ERROR - 2021-10-12 12:06:38 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', NULL, NULL, '25', '1', 'Physics', 'KG', '-', '10-40', 1, '0')
-ERROR - 2021-10-12 12:06:38 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', NULL, NULL, '25', '2', 'Chemistry', 'KG', '-', '10-40', 1, '0')
-ERROR - 2021-10-12 12:06:38 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('2', 'Nurophils', NULL, NULL, '20', '2', 'Chemistry', 'KG', '-', '10-40', 1, '0')
-ERROR - 2021-10-12 12:06:38 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', NULL, NULL, '20', '2', 'Chemistry', 'GM', '-', '10-40', 1, '0')
-ERROR - 2021-10-12 12:11:59 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', NULL, NULL, '10', '2', 'Chemistry', 'KG', 'Test one Test', '10-40', 2, '1')
-ERROR - 2021-10-12 12:11:59 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('2', 'Nurophils', NULL, NULL, '25', '2', 'Chemistry', 'KG', 'Test one Test', '10-40', 2, '1')
-ERROR - 2021-10-12 12:11:59 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', NULL, NULL, '2', '2', 'Chemistry', 'GM', '-', '10-40', 2, '0')
-ERROR - 2021-10-12 12:11:59 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', NULL, NULL, '25', '1', 'Physics', 'GM', '-', '10-40', 2, '0')
-ERROR - 2021-10-12 12:11:59 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', NULL, NULL, '25', '1', 'Physics', 'KG', '-', '10-40', 2, '0')
-ERROR - 2021-10-12 12:11:59 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', NULL, NULL, '25', '2', 'Chemistry', 'KG', '-', '10-40', 2, '0')
-ERROR - 2021-10-12 12:11:59 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('2', 'Nurophils', NULL, NULL, '20', '2', 'Chemistry', 'KG', '-', '10-40', 2, '0')
-ERROR - 2021-10-12 12:11:59 --> Query error: Column 'testId' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', NULL, NULL, '20', '2', 'Chemistry', 'GM', '-', '10-40', 2, '0')
-ERROR - 2021-10-12 12:18:16 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:18:17 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:18:18 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:18:19 --> 404 Page Not Found: Images/favicon.ico
-ERROR - 2021-10-12 12:18:51 --> Query error: Column 'testName' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', '2', NULL, '20', '2', 'Chemistry', 'KG', 'Test one Test', '10-40', 4, '1')
-ERROR - 2021-10-12 12:18:51 --> Query error: Column 'testName' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('2', 'Nurophils', '2', NULL, '10', '2', 'Chemistry', 'KG', 'Test one Test', '10-40', 4, '1')
-ERROR - 2021-10-12 12:18:51 --> Query error: Column 'testName' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', '2', NULL, '00', '2', 'Chemistry', 'GM', '-', '10-40', 4, '0')
-ERROR - 2021-10-12 12:18:51 --> Query error: Column 'testName' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', '3', NULL, '58', '1', 'Physics', 'GM', '-', '10-40', 4, '0')
-ERROR - 2021-10-12 12:18:51 --> Query error: Column 'testName' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', '3', NULL, '58', '1', 'Physics', 'KG', '-', '10-40', 4, '0')
-ERROR - 2021-10-12 12:18:51 --> Query error: Column 'testName' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('1', 'Basophils', '4', NULL, '01', '2', 'Chemistry', 'KG', '-', '10-40', 4, '0')
-ERROR - 2021-10-12 12:18:51 --> Query error: Column 'testName' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('2', 'Nurophils', '4', NULL, '15', '2', 'Chemistry', 'KG', '-', '10-40', 4, '0')
-ERROR - 2021-10-12 12:18:51 --> Query error: Column 'testName' cannot be null - Invalid query: INSERT INTO `case_report_data` (`parameterId`, `parameter`, `testId`, `testName`, `finding_value`, `categoryid`, `category`, `unit`, `label`, `reference_value`, `reportId`, `isgroup`) VALUES ('3', 'Entomology', '4', NULL, '14', '2', 'Chemistry', 'GM', '-', '10-40', 4, '0')
-ERROR - 2021-10-12 12:25:20 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:25:40 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:25:40 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:25:58 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:25:58 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:26:07 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:26:07 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:26:14 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:26:14 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:27:30 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:27:30 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:27:44 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:27:46 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:27:46 --> 404 Page Not Found: Resource/assets
-ERROR - 2021-10-12 12:27:47 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:16:06 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:16:06 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:16:09 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:16:09 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:16:16 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:16:16 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:16:16 --> 404 Page Not Found: Invoice/generate_invoice_bg.jpg
+ERROR - 2021-10-12 13:16:20 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:16:20 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 13:42:10 --> Severity: Warning --> strpos() expects parameter 1 to be string, array given D:\XAMPP\htdocs\CI_smartlab\application\models\Reciept_model.php 286
+ERROR - 2021-10-12 13:42:10 --> Severity: Warning --> strpos() expects parameter 1 to be string, array given D:\XAMPP\htdocs\CI_smartlab\application\models\Reciept_model.php 286
+ERROR - 2021-10-12 13:42:10 --> Severity: Warning --> strpos() expects parameter 1 to be string, array given D:\XAMPP\htdocs\CI_smartlab\application\models\Reciept_model.php 286
+ERROR - 2021-10-12 13:42:10 --> Severity: Warning --> strpos() expects parameter 1 to be string, array given D:\XAMPP\htdocs\CI_smartlab\application\models\Reciept_model.php 286
+ERROR - 2021-10-12 13:42:10 --> Severity: Warning --> strpos() expects parameter 1 to be string, array given D:\XAMPP\htdocs\CI_smartlab\application\models\Reciept_model.php 286
+ERROR - 2021-10-12 13:42:10 --> Severity: Warning --> strpos() expects parameter 1 to be string, array given D:\XAMPP\htdocs\CI_smartlab\application\models\Reciept_model.php 286
+ERROR - 2021-10-12 13:42:10 --> Severity: Warning --> strpos() expects parameter 1 to be string, array given D:\XAMPP\htdocs\CI_smartlab\application\models\Reciept_model.php 286
+ERROR - 2021-10-12 13:42:10 --> Severity: Warning --> strpos() expects parameter 1 to be string, array given D:\XAMPP\htdocs\CI_smartlab\application\models\Reciept_model.php 286
+ERROR - 2021-10-12 14:33:13 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 14:33:14 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-10-12 14:38:54 --> 404 Page Not Found: Resource/assets
