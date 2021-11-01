ee1753bbcce6f1394771068a64866b72048a1d1e
vbpawar <vikaspawar3110@gmail.com>
2021-11-01 18:31:40 +0530

    add commit

---
+++ application/logs/log-2021-11-01.php
@@ -0,0 +1,56 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-11-01 18:09:36 --> Query error: Unknown column 'crd.level' in 'field list' - Invalid query: SELECT crd.case_report_id,crd.reportId,crd.parameterId,crd.parameter,crd.testId,crd.testName,
+        crd.finding_value,crd.level,crd.categoryid,crd.category,crd.unit,crd.reference_value,crd.label,crd.isgroup 
+        FROM case_report_data crd INNER JOIN case_report_master crm ON crm.reportId = crd.reportId
+        WHERE crm.caseId = 8
+ERROR - 2021-11-01 18:09:54 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:09:54 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:09:54 --> 404 Page Not Found: Create_casejpg/index
+ERROR - 2021-11-01 18:10:17 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:10:17 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:10:17 --> 404 Page Not Found: Create_casejpg/index
+ERROR - 2021-11-01 18:10:23 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:10:23 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:10:40 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:10:40 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:10:40 --> 404 Page Not Found: Invoice/generate_invoice_bg.jpg
+ERROR - 2021-11-01 18:11:12 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:11:12 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:14:14 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:14:14 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:25:43 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:25:44 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:26:44 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:26:44 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:26:49 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:26:49 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:27:00 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:27:00 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:27:05 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:27:05 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:06 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:06 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:11 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:12 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:25 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:25 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:29 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:29 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:44 --> Severity: Warning --> Invalid argument supplied for foreach() D:\XAMPP\htdocs\CI_smartlab\application\models\TestPanelModel.php 69
+ERROR - 2021-11-01 18:28:44 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:45 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:57 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:28:57 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:29:01 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:29:01 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:29:26 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:29:26 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:29:32 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:29:32 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:30:55 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:30:55 --> 404 Page Not Found: Create_casejpg/index
+ERROR - 2021-11-01 18:30:55 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:31:18 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:31:19 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-01 18:31:19 --> 404 Page Not Found: Create_casejpg/index
