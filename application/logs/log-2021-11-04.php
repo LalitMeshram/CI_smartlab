5d6bbd362b02b04fe37e22e61514a23cd9455f5f
lalit <lalit@DESKTOP-077QKLL>
2021-11-04 12:47:37 +0530

    update enterfinding

---
+++ application/logs/log-2021-11-04.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-11-04 12:40:50 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-04 12:40:51 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-04 12:45:53 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-04 12:45:54 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-04 12:46:29 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-04 12:46:30 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-04 12:47:23 --> 404 Page Not Found: Resource/assets
