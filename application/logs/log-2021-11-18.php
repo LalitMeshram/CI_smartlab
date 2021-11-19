d9a42a8a567cfa323ba0a3c738afefad6a912837
vbpawar <vikaspawar3110@gmail.com>
2021-11-19 12:42:37 +0530

    add changes

---
+++ application/logs/log-2021-11-18.php
@@ -0,0 +1,91 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-11-18 16:31:17 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:17 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:26 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:26 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:26 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:26 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:26 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:26 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:26 --> Query error: Access denied for user 'smartlab_uat'@'localhost' (using password: YES) - Invalid query: SELECT * FROM customer_registeration
+         WHERE emailId = 'johnsmith@gmail.com' AND upassword='12345' AND isactive=1
+ERROR - 2021-11-18 16:31:26 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMPP\htdocs\CI_smartlab\application\models\LoginModel.php 10
+ERROR - 2021-11-18 16:31:26 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:26 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:26 --> Query error: Access denied for user 'smartlab_uat'@'localhost' (using password: YES) - Invalid query: SELECT * FROM customer_registeration
+         WHERE emailId = 'johnsmith@gmail.com' AND upassword='12345' AND isactive=1
+ERROR - 2021-11-18 16:31:26 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMPP\htdocs\CI_smartlab\application\models\LoginModel.php 10
+ERROR - 2021-11-18 16:31:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:28 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:28 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:28 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:28 --> Query error: Access denied for user 'smartlab_uat'@'localhost' (using password: YES) - Invalid query: SELECT * FROM customer_registeration
+         WHERE emailId = 'johnsmith@gmail.com' AND upassword='12345' AND isactive=1
+ERROR - 2021-11-18 16:31:28 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMPP\htdocs\CI_smartlab\application\models\LoginModel.php 10
+ERROR - 2021-11-18 16:31:28 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:28 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:28 --> Query error: Access denied for user 'smartlab_uat'@'localhost' (using password: YES) - Invalid query: SELECT * FROM customer_registeration
+         WHERE emailId = 'johnsmith@gmail.com' AND upassword='12345' AND isactive=1
+ERROR - 2021-11-18 16:31:28 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMPP\htdocs\CI_smartlab\application\models\LoginModel.php 10
+ERROR - 2021-11-18 16:31:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:33 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:33 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:33 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:33 --> Query error: Access denied for user 'smartlab_uat'@'localhost' (using password: YES) - Invalid query: SELECT * FROM customer_registeration
+         WHERE emailId = 'johnsmith@gmail.com' AND upassword='12345' AND isactive=1
+ERROR - 2021-11-18 16:31:33 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMPP\htdocs\CI_smartlab\application\models\LoginModel.php 10
+ERROR - 2021-11-18 16:31:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:33 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:33 --> Query error: Access denied for user 'smartlab_uat'@'localhost' (using password: YES) - Invalid query: SELECT * FROM customer_registeration
+         WHERE emailId = 'johnsmith@gmail.com' AND upassword='12345' AND isactive=1
+ERROR - 2021-11-18 16:31:33 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMPP\htdocs\CI_smartlab\application\models\LoginModel.php 10
+ERROR - 2021-11-18 16:31:34 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:34 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:34 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:34 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:34 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:34 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:34 --> Query error: Access denied for user 'smartlab_uat'@'localhost' (using password: YES) - Invalid query: SELECT * FROM customer_registeration
+         WHERE emailId = 'johnsmith@gmail.com' AND upassword='12345' AND isactive=1
+ERROR - 2021-11-18 16:31:34 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMPP\htdocs\CI_smartlab\application\models\LoginModel.php 10
+ERROR - 2021-11-18 16:31:35 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'smartlab_uat'@'localhost' (using password: YES) D:\XAMPP\htdocs\CI_smartlab\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-18 16:31:35 --> Unable to connect to the database
+ERROR - 2021-11-18 16:31:35 --> Query error: Access denied for user 'smartlab_uat'@'localhost' (using password: YES) - Invalid query: SELECT * FROM customer_registeration
+         WHERE emailId = 'johnsmith@gmail.com' AND upassword='12345' AND isactive=1
+ERROR - 2021-11-18 16:31:35 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMPP\htdocs\CI_smartlab\application\models\LoginModel.php 10
+ERROR - 2021-11-18 16:31:48 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:31:48 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:31:49 --> 404 Page Not Found: Resource/img
+ERROR - 2021-11-18 16:32:15 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:32:15 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:32:16 --> 404 Page Not Found: Resource/img
+ERROR - 2021-11-18 16:32:18 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:32:18 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:32:36 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:32:36 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:32:36 --> 404 Page Not Found: Create_casejpg/index
+ERROR - 2021-11-18 16:32:56 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:32:56 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:32:57 --> 404 Page Not Found: Create_casejpg/index
+ERROR - 2021-11-18 16:33:02 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:33:02 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:33:10 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:33:10 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:33:10 --> 404 Page Not Found: Create_casejpg/index
+ERROR - 2021-11-18 16:33:38 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:33:39 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:33:39 --> 404 Page Not Found: Invoice/generate_invoice_bg.jpg
+ERROR - 2021-11-18 16:33:40 --> 404 Page Not Found: Images/favicon.ico
+ERROR - 2021-11-18 16:33:59 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:34:00 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:34:00 --> 404 Page Not Found: Update_case/create_case.jpg
+ERROR - 2021-11-18 16:34:44 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:34:44 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:34:44 --> 404 Page Not Found: Invoice/generate_invoice_bg.jpg
+ERROR - 2021-11-18 16:35:11 --> 404 Page Not Found: Resource/assets
+ERROR - 2021-11-18 16:35:11 --> 404 Page Not Found: Resource/assets
